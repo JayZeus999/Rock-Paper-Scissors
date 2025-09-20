f2e4c0ce0909937003e688b8cee26911bed33d4a
Falade Joseph <faladejoseph400@gmail.com>
2025-09-20 18:06:06 +0100

    Declare rand

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ console.log("Hello World");
 
 function getComputerChoice(){
     let stuff = ("rock", "paper", "scissors")
-    return Math.round(Math.random()*2)
 }
 
+let rand = Math.round(Math.random()*2)
 console.log(getComputerChoice())
