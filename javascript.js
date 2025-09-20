9345e55702184b7c0f00338d1c5f7a271cf8c189
Falade Joseph <faladejoseph400@gmail.com>
2025-09-20 18:04:27 +0100

    Declare stuff

---
+++ javascript.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+
+function getComputerChoice(){
+    let stuff = ("rock", "paper", "scissors")
+    return Math.round(Math.random()*2)
+}
+
+console.log(getComputerChoice())
