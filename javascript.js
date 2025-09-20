12431946227490d895c617824b1d6a08beaf08d5
Falade Joseph <faladejoseph400@gmail.com>
2025-09-20 23:18:08 +0100

    Rewrite the lines of logic

--- javascript.js
+++ javascript.js
@@ -17,6 +17,18 @@ computerScore = 0;
 function playRound(humanChoice, computerChoice) {
   if (humanChoice == computerChoice){
     console.log("a draw, go again")
+  }else if (humanChoice === "rock" && computerChoice === "paper"){
+    console.log("you lose, paper wins")
+  }else if (humanChoice === "rock" && computerChoice === "scissors"){
+    console.log("you win, rock rules")
+  }else if (humanChoice === "paper" && computerChoice === "rock"){
+    console.log("you win, paper wins")
+  }else if (humanChoice === "paper" && computerChoice === "scissors"){
+    console.log("you lose, scissors wins")
+  }else if (humanChoice === "scissors" && computerChoice === "rock"){
+    console.log("you lose, rock wins")
+  }else if (humanChoice === "scissors" && computerChoice === "paper"){
+    console.log("you win, scissors wins")
   }
 }
 
