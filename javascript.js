f1b3c6817efcd48d84fec32fae04b7e1b3f6ef53
Falade Joseph <faladejoseph400@gmail.com>
2025-09-21 19:16:52 +0100

    Add conditionals to declare winner

--- javascript.js
+++ javascript.js
@@ -10,47 +10,56 @@ function getHumanChoice() {
 }
 
 
-function playGame(){
+function playGame() {
   let humanScore = 0;
   let computerScore = 0;
+  console.log(`The current human score is: ${humanScore}`)
+  console.log(`The current computer score is: ${computerScore}`)
 
   function playRound(humanChoice, computerChoice) {
 
     if ((humanChoice === "rock" && computerChoice === "scissors") ||
-      
-     (humanChoice === "paper" && computerChoice === "rock") ||
-      
-     (humanChoice === "scissors" && computerChoice === "paper") 
+
+      (humanChoice === "paper" && computerChoice === "rock") ||
+
+      (humanChoice === "scissors" && computerChoice === "paper")
     ) {
-      console.log(humanChoice);
-      console.log(computerChoice);
+      console.log(`The human choice is ${humanChoice}`);
+      console.log(`The computer choice is ${computerChoice}`);
       console.log("you win");
       humanScore += 1;
-      console.log(`The human score is: ${humanScore}`)
-      console.log(`The computer score is: ${computerScore}`)
+      console.log(`The current human score is: ${humanScore}`)
+      console.log(`The current computer score is: ${computerScore}`)
     }
 
 
-    else if ((humanChoice === "rock" && computerChoice === "paper") ||      
-    (humanChoice === "paper" && computerChoice === "scissors") ||
-    (humanChoice === "scissors" && computerChoice === "rock") ){
-      console.log(humanChoice);
-      console.log(computerChoice);
+    else if ((humanChoice === "rock" && computerChoice === "paper") ||
+      (humanChoice === "paper" && computerChoice === "scissors") ||
+      (humanChoice === "scissors" && computerChoice === "rock")) {
+      console.log(`The human choice is ${humanChoice}`);
+      console.log(`The computer choice is ${computerChoice}`);
       console.log("you lose");
       computerScore += 1;
-      console.log(`The human score is: ${humanScore}`)
-      console.log(`The computer score is: ${computerScore}`)
+      console.log(`The current human score is: ${humanScore}`)
+      console.log(`The current computer score is: ${computerScore}`)
     }
-    
-    else if (humanChoice === computerChoice){
+
+    else if (humanChoice === computerChoice) {
       console.log("draw");
     }
-    
+
   }
 
 
   playRound(getHumanChoice(), getComputerChoice());
   playRound(getHumanChoice(), getComputerChoice());
+  if (humanScore > computerScore) {
+    console.log("human wins");
+  } else if (computerScore > humanScore) {
+    console.log("computer wins");
+  }
+
 }
 
 playGame();
+
