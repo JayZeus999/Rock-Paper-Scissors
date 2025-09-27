f8c4c9bf5bcbdb295e75ae87aa8d582aea2f5899
Falade Joseph <faladejoseph400@gmail.com>
2025-09-27 21:23:55 +0100

    Add button event listeners

--- javascript.js
+++ javascript.js
@@ -10,63 +10,73 @@ function getHumanChoice() {
   let btn = document.createElement("button");
   let btn1 = document.createElement("button");
   let btn2 = document.createElement("button");
+
+  btn.textContent = "Rock";
+  btn1.textContent = "Paper";
+  btn2.textContent = "Scissors";
 }
 
-playRound(getHumanChoice(), getComputerChoice());
 
 // function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-  function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice) {
 
-    if ((humanChoice === "rock" && computerChoice === "scissors") ||
+  if ((humanChoice === "rock" && computerChoice === "scissors") ||
 
-      (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
 
-      (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
-      console.log(`The human choice is ${humanChoice}`);
-      console.log(`The computer choice is ${computerChoice}`);
-      console.log("you win");
-      humanScore += 1;
-      console.log(`The current human score is: ${humanScore}`)
-      console.log(`The current computer score is: ${computerScore}`)
-    }
-
-
-    else if ((humanChoice === "rock" && computerChoice === "paper") ||
-      (humanChoice === "paper" && computerChoice === "scissors") ||
-      (humanChoice === "scissors" && computerChoice === "rock")) {
-      console.log(`The human choice is ${humanChoice}`);
-      console.log(`The computer choice is ${computerChoice}`);
-      console.log("you lose");
-      computerScore += 1;
-      console.log(`The current human score is: ${humanScore}`)
-      console.log(`The current computer score is: ${computerScore}`)
-    }
-
-    else if (humanChoice === computerChoice) {
-      console.log(`The human choice is ${humanChoice}`);
-      console.log(`The computer choice is ${computerChoice}`)
-      console.log("draw");
-      console.log(`The current human score is: ${humanScore}`)
-      console.log(`The current computer score is: ${computerScore}`)
-    }
-
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log(`The human choice is ${humanChoice}`);
+    console.log(`The computer choice is ${computerChoice}`);
+    console.log("you win");
+    humanScore += 1;
+    console.log(`The current human score is: ${humanScore}`)
+    console.log(`The current computer score is: ${computerScore}`)
   }
 
 
-  // playRound(getHumanChoice(), getComputerChoice());
-  // playRound(getHumanChoice(), getComputerChoice());
-  // playRound(getHumanChoice(), getComputerChoice());
-  // playRound(getHumanChoice(), getComputerChoice());
-  // playRound(getHumanChoice(), getComputerChoice());
-  if (humanScore > computerScore){
+  else if ((humanChoice === "rock" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "rock")) {
+    console.log(`The human choice is ${humanChoice}`);
+    console.log(`The computer choice is ${computerChoice}`);
+    console.log("you lose");
+    computerScore += 1;
+    console.log(`The current human score is: ${humanScore}`)
+    console.log(`The current computer score is: ${computerScore}`)
+  }
+
+  else if (humanChoice === computerChoice) {
+    console.log(`The human choice is ${humanChoice}`);
+    console.log(`The computer choice is ${computerChoice}`)
+    console.log("draw");
+    console.log(`The current human score is: ${humanScore}`)
+    console.log(`The current computer score is: ${computerScore}`)
+  }
+
+}
+
+
+// playRound(getHumanChoice(), getComputerChoice());
+// playRound(getHumanChoice(), getComputerChoice());
+// playRound(getHumanChoice(), getComputerChoice());
+// playRound(getHumanChoice(), getComputerChoice());
+// playRound(getHumanChoice(), getComputerChoice());
+
+btn.addEventListener("click", () => {
+  playRound(getHumanChoice(), getComputerChoice());
+});
+
+
+if (humanScore > computerScore) {
   console.log("human wins");
-  }else if (computerScore > humanScore){
+} else if (computerScore > humanScore) {
   console.log("computer wins");
 }
+
 
 // }
 
